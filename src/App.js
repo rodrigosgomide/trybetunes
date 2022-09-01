dd9b05002195503827d9ab576836458754dc8816
rodrigosgomide <rodrigogomide.def@gmail.com>
2022-09-01 14:56:26 -0300

    R2

--- src/App.js
+++ src/App.js
@@ -17,13 +17,15 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" component={ NotFound } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="" component={ NotFound } />
+        </Switch>
       </Router>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,68 @@
 import React, { Component } from 'react';
+import { PropTypes, history as historyPropTypes } from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends Component {
+  state = {
+    newUserName: '',
+    checkButton: true,
+    loading: false,
+  };
+
+  handlerUsername = (event) => {
+    const { value } = event.target;
+    const minUserCharacteres = 3;
+    this.setState({ newUserName: value });
+    if (value.length >= minUserCharacteres) {
+      this.setState({ checkButton: false });
+    } else {
+      this.setState({ checkButton: true });
+    }
+  };
+
+  handlerButton = (event) => {
+    event.preventDefault();
+    const { newUserName } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true });
+    createUser({ name: newUserName })
+      .then(() => {
+        this.setState({ loading: false });
+        history.push('/search');
+      });
+  };
+
   render() {
+    const { checkButton, loading } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form action="">
+          <label htmlFor="userName">
+            Nome:
+            <input
+              data-testid="login-name-input"
+              type="text"
+              onChange={ this.handlerUsername }
+            />
+          </label>
+          <button
+            data-testid="login-submit-button"
+            disabled={ checkButton }
+            onClick={ this.handlerButton }
+            type="submit"
+          >
+            Entrar
+          </button>
+          {loading && <Loading />}
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape(historyPropTypes).isRequired,
+};
 
 export default Login;
